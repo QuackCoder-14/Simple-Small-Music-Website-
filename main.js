f1871f7efe0805bf334402947f440ea76c65d7d9
QuackCoder-14 <omganeshpatil@gmail.com>
2023-09-05 20:14:51 +0530

    files are added

---
+++ main.js
@@ -0,0 +1,13 @@
+// document.querySelector('.btn').onclick = () => {
+//     document.querySelector('.container').classList.add('active');
+// }
+// document.querySelector('.close-btn').onclick = () => {
+//     document.querySelector('.container').classList.remove('active');
+// }
+
+document.querySelector('.btn').onclick = () => {
+    location.href = "musiclist.html";
+    }
+    // document.getElementsByClassName(".btn").onclick = function () {
+    //     location.href = "musiclist.html";
+    // };
